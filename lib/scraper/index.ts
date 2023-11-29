e19e653921fb67f79a33a01058a5078de9b3eb24
Christine Wong <jumiryoo@gmail.com>
2023-11-29 01:17:48 -0800

    pushing edits

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -7,8 +7,8 @@ export async function scrapeAmazonProduct(url: string) {
 
 
     // BrightData proxy configuration
-    const username = String(process.env.BRIGHT_DATA_USERNAME);
-    const password = String(process.env.BRIGHT_DATA_PASSWORD);
+    const username = String(process.env.NEXT_PUBLIC_BRIGHT_DATA_USERNAME);
+    const password = String(process.env.NEXT_PUBLIC_BRIGHT_DATA_PASSWORD);
     const port = 22225;
     const session_id = (1000000 * Math.random()) | 0;
 
